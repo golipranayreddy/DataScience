5a5a31039038d157b4f997fe82617624b6aca675
golipranayreddy <golipranayreddy@msitprogram.net>
2021-06-14 17:53:19 +0530

    done

---
+++ DADV/Assignment-3/scatterplot.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+
+
+
+data =  pd.read_csv('C:/Users/DELL/Desktop/task3/rank_votes.csv')
+final_data = data[data['numVotes'] >= 10000 ]
+
+
+sns_plot  = sns.jointplot(x = final_data["averageRating"], y = final_data["numVotes"], kind = 'scatter')
+sns_plot.savefig("scatterplot.svg")
